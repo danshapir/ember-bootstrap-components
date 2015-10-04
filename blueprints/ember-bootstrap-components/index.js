6ffb420964db09cc98904146be21dbe2f32e264a
danshapir <s.dan@live.com>
2015-10-04 18:51:54 +0300

    Add scss addon

--- blueprints/ember-bootstrap-components/index.js
+++ blueprints/ember-bootstrap-components/index.js
@@ -3,6 +3,9 @@ module.exports = {
    },
 
    afterInstall: function() {
-       return this.addBowerPackagesToProject( [{name: 'bootstrap', target: 'latest'}, {name:'waves', target:'^0.7.2'}]);
+      var _this = this;
+      return this.addBowerPackagesToProject( [{name: 'bootstrap', target: 'latest'}, {name:'waves', target:'^0.7.2'}]).then(function() {
+        return _this.addPackagesToProject([{name: 'ember-cli-sass', target: 'latest'}]);
+      });;
    }
 };
