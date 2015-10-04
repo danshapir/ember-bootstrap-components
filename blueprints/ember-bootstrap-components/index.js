5ab41387300012b89765901d602d654c78510953
danshapir <s.dan@live.com>
2015-10-04 19:15:21 +0300

    addAddonToProject

--- blueprints/ember-bootstrap-components/index.js
+++ blueprints/ember-bootstrap-components/index.js
@@ -5,7 +5,9 @@ module.exports = {
    afterInstall: function() {
       var _this = this;
       return this.addBowerPackagesToProject( [{name: 'bootstrap', target: 'latest'}, {name:'waves', target:'^0.7.2'}]).then(function() {
-        return _this.addPackagesToProject([{name: 'ember-cli-sass', target: 'latest'}]);
+        return _this.addAddonToProject({name: 'ember-cli-sass', target: 'latest'}).then(function () {
+            return _this.addAddonToProject( {name: 'ember-cli-bourbon', target:'latest'});
+        });
       });;
    }
 };
